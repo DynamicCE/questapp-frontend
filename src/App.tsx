a74cbeda9b8407f9344a4ec01a20e0bd0beb31f1
Adınız <erkanmurat0632@gmail.com>
2024-06-29 23:04:42 +0300

    bugs

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { UserPage } from './pages/UserPage';
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <h1>new page</h1>
+      <UserPage />
     </div>
   );
 }
---
+++ src/pages/UserPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { UserList } from '../components/comment/UserList';
+
+export const UserPage: React.FC = () => {
+  return (
+    <div>
+      <h1>Kullanıcı Sayfası</h1>
+      <UserList />
+    </div>
+  );
+};
