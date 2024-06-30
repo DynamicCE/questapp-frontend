19d78cc3ba33e5f26e762f81bbc7b15bf0fa23f4
Adınız <erkanmurat0632@gmail.com>
2024-06-30 20:06:48 +0300

    updates

--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -1,1 +1,13 @@
+import React from 'react';
+
+const RegisterPage = () => {
+  return (
+    <div>
+      <h1>RegisterPage</h1>
+      {/* Login form veya diğer içerikler buraya gelecek */}
+    </div>
+  );
+};
+
+export default RegisterPage;
 export {};
--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import LoginPage from '../pages/LoginPage';
 import HomePage from '../pages/HomePage';
-
+import RegisterPage from '../pages/RegisterPage';
 const AppRoutes = () => {
   return (
     <Routes>
        <Route path="/login" element={<LoginPage />} />
        <Route path='/home' element= {<HomePage/>}/>
+    <Route path='/register' element= {<RegisterPage/>}/>
     </Routes>
   );
 };
