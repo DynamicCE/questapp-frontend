5e6f98e1a6628a3c5efcc1d79319fb362c2a9e2e
Adınız <erkanmurat0632@gmail.com>
2024-07-02 18:14:05 +0300

    update

--- src/components/post/Post.tsx
+++ src/components/post/Post.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 export interface PostType {
   id: number;
@@ -8,21 +8,27 @@ export interface PostType {
   username: string;
   content?: string;
   likeCount?: number;
-    profilePictureUrl: string;
+  profilePictureUrl: string;
 }
 
-const Post: React.FC<PostType> = ({ title, text, username, content, likeCount,profilePictureUrl }) => {
+const Post: React.FC<PostType> = ({
+  title,
+  text,
+  username,
+  content,
+  likeCount,
+  profilePictureUrl,
+}) => {
   return (
     <div className="bg-white shadow-md rounded-lg p-4 mb-4">
-<img
-          src={profilePictureUrl}
-          alt={`${username}'s profile`}
-          className="w-10 h-10 rounded-full mr-4"
-        />
+      <img
+        src={profilePictureUrl}
+        alt={`${username}'s profile`}
+        className="w-10 h-10 rounded-full mr-4"
+      />
       <h2 className="text-xl font-bold mb-2">{title}</h2>
       <p className="text-gray-700 mb-4">{text || content}</p>
       <div className="flex justify-between items-center">
-
         {likeCount !== undefined && (
           <span className="text-sm text-gray-500">Beğeni: {likeCount}</span>
         )}
